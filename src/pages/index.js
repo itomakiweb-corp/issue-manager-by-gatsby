bf46e25fcc0c6b09821eb204ecd3155427d5c5c4
Hide Charo (ひで) <59493482+hidecharo@users.noreply.github.com>
2020-01-16 06:43:31 +0900

    mod pages/index, del unused files

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,23 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h2>New Issue</h2>
+    <form>
+      <ul>
+        <li><input type="text" name="title" placeholder="title" /></li>
+        <li><textarea name="body" placeholder="body"></textarea></li>
+        <li><button type="submit">create new issue</button></li>
+      </ul>
+    </form>
+    <h2>Issues</h2>
+    <ul>
+      <li>issue title</li>
+    </ul>
   </Layout>
 )
 
