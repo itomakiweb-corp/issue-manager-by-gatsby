5109ab4d1d090a3146f059cbd09a1757f0e1446e
Hide Charo (ひで) <59493482+hidecharo@users.noreply.github.com>
2020-01-16 08:06:54 +0900

    add handleSubmit base, useState

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,32 @@
-import React from "react"
+import React, { useState } from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = ({ data }) => (
-  <Layout>
+const IndexPage = ({ data }) => {
+  const [title, setTitle] = useState("")
+  const [body, setBody] = useState("- [ ] \n- [ ] \n- [ ] \n")
+
+  const handleSubmit = (e) => {
+    // stop page transition after submit
+    e.preventDefault()
+
+    const payload = {
+      title,
+      body,
+    }
+    createGithubIssue(payload)
+  }
+
+  return <Layout>
     <SEO title="Home" />
+
     <h2>New Issue</h2>
-    <form>
+    <form onSubmit={handleSubmit}>
       <ul>
-        <li><input type="text" name="title" placeholder="title" /></li>
-        <li><textarea name="body" placeholder="body"></textarea></li>
+        <li><input type="text" name="title" placeholder="title" value={title} onChange={(e) => setTitle(e.target.value)} /></li>
+        <li><textarea name="body" placeholder="body" value={body} onChange={(e) => setBody(e.target.value)} /></li>
         <li><button type="submit">create new issue</button></li>
       </ul>
     </form>
@@ -19,12 +34,16 @@ const IndexPage = ({ data }) => (
     <ul>
       {data.github.repository.issues.nodes.map((issue, i) => (
         <li key={issue.id}>
-          <a href={issue.url} target="_blank">{i + 1}: {issue.title}</a>
+          <a href={issue.url} target="_blank" rel="noopener noreferrer">{i + 1}: {issue.title}</a>
         </li>
       ))}
     </ul>
   </Layout>
-)
+}
+
+const createGithubIssue = (payload) => {
+  console.log(payload)
+}
 
 export const query = graphql`
   query {
